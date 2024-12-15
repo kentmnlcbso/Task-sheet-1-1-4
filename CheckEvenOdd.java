42be50afe060337d9913e72d255ce3d2b49bc66c
Kenneth Emanuel Cabusao <119332036+kentmnlcbso@users.noreply.github.com>
2024-12-15 14:02:39 +0000

    Done adding output

--- CheckEvenOdd.java
+++ CheckEvenOdd.java
@@ -1,1 +1,15 @@
+public class CheckEvenOdd {
 
+    public static void main(String[] args) {
+        int check_number = 10;
+
+        String message;
+
+        for (int i = 1; i <= check_number; i++) {
+
+            message = (i % 2 == 0) ? i + " is an even number" : i + " is an odd number";
+
+            System.out.println(message);
+        }
+    }
+}
